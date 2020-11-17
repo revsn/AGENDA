133e744d2b9e007f80663fbf98fc3da0e0888e5d
ZAC WEAVER <33301232+zcwe@users.noreply.github.com>
2020-11-17 16:57:45 -0500

    fix safari issue (#12) (#13)
    * fix safari issue
    
    * update profile

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,1 @@
-AOS.init();
-
-new TypeIt('.type-effect', {
-    // options
-}).type("Hy ").pause(1000).move(-1).delete({delay:600}).type("ey, ").pause(600).type("what's on today's agenda ?").go();
+const isChrome = navigator.userAgent.indexOf("Chrome") > -1;
